4ad427df080a2ccf6dec79e69cff3b1a6f4339b3
giljulian <julian.gil@applydigital.com>
2023-08-16 10:24:47 -0400

    Initial commit

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ const questions = [
 
 inquirer.prompt(questions).then(async (answers) => {
   const { template, projectName } = answers;
-
-  console.log({ answers });
   
   const templatePath = path.join(packagesPath, `${template}-boilerplate`);
   const projectFolderPath = path.join(generatedProjectsPath, projectName); // New folder path
@@ -77,17 +75,8 @@ inquirer.prompt(questions).then(async (answers) => {
     await repo.add('.'); // Add all files
     await repo.commit('Initial commit'); // Commit with a message
 
-    const remotes = await repo.getRemotes(); // Get existing remotes
-    const originRemoteExists = remotes.some((remote) => remote.name === 'origin');
-
-    if (!originRemoteExists) {
-      // Add the remote GitHub repository
-      console.log({repoName});
-      await repo.addRemote('origin', `https://github.com/${GITHUB_USERNAME}/${repoName}.git`);
-    }
-
     // Add the remote GitHub repository
-    // await repo.addRemote('origin', `git@github.com:${GITHUB_USERNAME}/${repoName}.git`);
+    await repo.addRemote('origin', `git@github.com:${GITHUB_USERNAME}/${repoName}.git`);
 
     // Push to the remote repository
     await repo.push('origin', 'main');
