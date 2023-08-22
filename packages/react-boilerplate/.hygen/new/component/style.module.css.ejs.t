a5a2daa5d2017b32b8e649388572e1d3cb8c65b6
giljulian <142046817+giljulian@users.noreply.github.com>
2023-08-22 14:11:29 -0400

    Revert "Feat/add hygen"

--- packages/react-boilerplate/.hygen/new/component/style.module.css.ejs.t
+++
@@ -1,4 +0,0 @@
----
-to: <%= absPath %>/style.module.css
----
-.container {}
--- packages/react-boilerplate/src/components/atoms/button/__tests__/button.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { button } from '../';
-
-test('renders component successfully', () => {
-  render(<button  />);
-  const element = screen.getByTestId(/test/i);
-  expect(element).toBeInTheDocument();
-});
